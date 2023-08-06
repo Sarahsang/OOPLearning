aa2e26e96ace171a1d4e1aaca05e905215a3f1a4
Miao Sang <118147601+Sarahsang@users.noreply.github.com>
2023-08-06 12:48:28 +1200

    discount.py

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # OOPLearning
+class可以单独添加 key和value （laptop例子，加discount）
+> m4800.bluetooth = 'yes'
+> 
+> print(m4800.__dict__)
+> 
+> {'name': 'm4800', 'price': 55000, 'bluetooth': 'yes'}
--- discount.py
+++ discount.py
@@ -3,7 +3,15 @@ class laptop:
         self.brand_name = brand_name
         self.price = price
         self.laptop_name = brand_name + ' ' + price
-        
+
+#creating object        
 m545 = laptop('HP','50000')
 
 print(m545.__dict__.keys())
+
+m4800 = laptop('Dell','60000')
+
+#adding new attribute to m4800
+m4800.bluetooth = 'yes'
+
+print(m4800.__dict__)
