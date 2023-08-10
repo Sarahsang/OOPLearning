67331a955f49d118208e60906117cbca78ece1ad
Miao Sang <118147601+Sarahsang@users.noreply.github.com>
2023-08-10 13:06:21 +1200

    Update person.py

--- person.py
+++ person.py
@@ -1,18 +1,26 @@
 class Person():
-    def __init__(self, name, age):
+    def __init__(self, name, age, height):
         self.name = name
         self.age = age
+        self.height = height
 
     def show(self):
         print(self.name, self.age)
     def speak(self):
         return f"Hello, my name is {self.name} and I am {self.age} years old"
     
-person1 = Person("John", 36)
-person2 = Person("Mary", 25)
-person3 = Person("Bob", 27)
-person4 = Person("Jane", 21)
-person5 = Person("Peter", 29)
+def HeightCheck(aHeight):
+    for student in students:
+        if student.height > aHeight:
+            print(f"{student.name} is taller than {aHeight}")
+        else:
+            print(f"{student.name} is shorter than {aHeight}")
+    
+person1 = Person("John", 36, 1.75)
+person2 = Person("Mary", 25, 1.65)
+person3 = Person("Bob", 27, 1.80)
+person4 = Person("Jane", 21, 1.70)
+person5 = Person("Peter", 29, 1.68)
 
 students = []
 students.append(person1)
@@ -22,5 +30,13 @@ students.append(person4)
 students.append(person5)
 
 #pop移除并返回被移除的元素
-PersonPop = students.pop(1)
-print(PersonPop.speak())
+# PersonPop = students.pop(1)
+# print(PersonPop.speak())
+
+#check height
+HeightCheck(1.70)
+
+print(students)
+
+for student in students:
+    student.show()
